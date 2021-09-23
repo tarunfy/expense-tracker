4ff07319fdbf465d2a00bc5d4cc4b710f82c13c3
tarun2506 <tarunsharma8920@gmail.com>
2021-09-23 21:38:49 +0530

    changes

---
+++ src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div>
+      <h1
+        className="text-center text-2xl text-blue-500
+      "
+      >
+        Expenseeeeeeeeee
+      </h1>
+    </div>
+  );
+}
+
+export default App;
