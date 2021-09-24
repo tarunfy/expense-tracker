ef9c930513445cd37b482c8e93e73e30b9a4ce20
tarun2506 <tarunsharma8920@gmail.com>
2021-09-24 14:37:37 +0530

    login

--- src/pages/LogIn.js
+++ src/pages/LogIn.js
@@ -1,7 +1,57 @@
 import React from "react";
 
-function LogIn() {
-  return <div className="text-center text-4xl text-primary">Login</div>;
+function Login() {
+  return (
+    <div className="container flex flex-col justify-center h-hero  mx-auto items-center">
+      <h1 className="font-Roboto font-semibold text-center text-gray-600 text-9xl mb-6">
+        Welcome Back
+      </h1>
+      <div className="bg-white w-96 pt-10 pb-8 px-10 shadow-2xl rounded-lg">
+        <form className="mb-0 space-y-6">
+          <div>
+            <div className="mb-2">
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Email Address
+              </label>
+              <div className="mt-1">
+                <input
+                  type="email"
+                  id="email"
+                  required
+                  className="w-full border border-gray-300 px-3 py-2 rounded-lg shadow-sm focus:outline-none focus:border-indigo-600 focus:ring-1"
+                />
+              </div>
+            </div>
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Password
+            </label>
+            <div className="mt-1">
+              <input
+                type="password"
+                id="password"
+                required
+                className="w-full border border-gray-300 px-3 py-2 rounded-lg shadow-sm focus:outline-none focus:border-indigo-600 focus:ring-1"
+              />
+            </div>
+          </div>
+          <div>
+            <button
+              type="submit"
+              className="w-full flex justify-center py-3 px-4 border border-transparent shadow-sm bg-primary hover:opacity-95 font-Roboto font-medium text-white text-center text-lg rounded-lg focus:ring-2 focus:outline-none focus:ring-offset-2 focus:ring-indigo-600 hover:shadow-md "
+            >
+              Log in
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
 
-export default LogIn;
+export default Login;
