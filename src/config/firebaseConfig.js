bff44daaedacd28b6c6bdb543168f041943690f4
Ronald Flores Sequeira <ronald@ronaldsequeira.com>
2021-10-25 15:07:22 -0600

    Update firebaseConfig.js
    Update firebaseConfig.js to remove firebase sensible access keys

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -2,14 +2,14 @@ import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
 
-// Your web app's Firebase configuration
+// Your web app's Firebase configuration from .env variables. 
 const firebaseConfig = {
-  apiKey: "AIzaSyC9i3F7YaRtK-751AVJf2jHluKNl8pQXF8",
-  authDomain: "expense-tracker-e3923.firebaseapp.com",
-  projectId: "expense-tracker-e3923",
-  storageBucket: "expense-tracker-e3923.appspot.com",
-  messagingSenderId: "738168370888",
-  appId: "1:738168370888:web:98bd3a43b2679ed71d57fd",
+  apiKey: proces.env.API_KEY,
+  authDomain: proces.env.AUTH_DOMAIN,
+  projectId: proces.env.PROJECT_ID,
+  storageBucket: proces.env.STORAGE_BUCKET,
+  messagingSenderId: proces.env.MESSAGING_SENDER_ID,
+  appId: proces.env.APP_ID,
 };
 
 // Initialize Firebase
